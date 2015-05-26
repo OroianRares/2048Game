2531b691fa74789c0ccce7e98b22832a0057510d
Rares Oroian <oroian.rares@gmail.com>
2015-05-26 17:14:39 +0300

    Game over functionality added.

--- animation.js
+++ animation.js
@@ -2,26 +2,40 @@ $(document).ready( function () {
     /* some initial variables*/
     var divMatrix = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
     var divCnt = 0;
+    var gameOver = false;
 
     var positionGenerator = new RandomPosGenerator();
 
-    /*
-    for (var i = 0; i < 16; i++) {
-        var randPos = positionGenerator.generateRandomFreePosition();
-        positionGenerator.removeFreePosition(randPos);
-        console.log(randPos);
-    }*/
+    var announceGameOver = function () {
+        console.log("GAME OVER");
+        gameOver = true;
+
+        divMatrix[1][0].$div.text('G'); divMatrix[1][0].$div.attr('class', 'gameOver');
+        divMatrix[1][1].$div.text('A'); divMatrix[1][1].$div.attr('class', 'gameOver');
+        divMatrix[1][2].$div.text('M'); divMatrix[1][2].$div.attr('class', 'gameOver');
+        divMatrix[1][3].$div.text('E'); divMatrix[1][3].$div.attr('class', 'gameOver');
+
+        divMatrix[2][0].$div.text('O'); divMatrix[2][0].$div.attr('class', 'gameOver');
+        divMatrix[2][1].$div.text('V'); divMatrix[2][1].$div.attr('class', 'gameOver');
+        divMatrix[2][2].$div.text('E'); divMatrix[2][2].$div.attr('class', 'gameOver');
+        divMatrix[2][3].$div.text('R'); divMatrix[2][3].$div.attr('class', 'gameOver');
+    };
+
 
     var spawnNewDiv = function () {
         var randPos = positionGenerator.generateRandomFreePosition();
-        positionGenerator.removeFreePosition(randPos);
-        var division = new Division(randPos);
+        if (randPos >= 0) {
+            positionGenerator.removeFreePosition(randPos);
+            var division = new Division(randPos);
 
-        /* add division to matrix */
-        divMatrix[division.y][division.x] = division; // add object to game grid
-        divCnt++;
-        /* set division state to visible */
-        division.setVisible(true);
+            /* add division to matrix */
+            divMatrix[division.y][division.x] = division; // add object to game grid
+            divCnt++;
+            /* set division state to visible */
+            division.setVisible(true);
+        } else {
+            announceGameOver();
+        }
     };
 
     for (var i = 0; i < 2; i++){
@@ -50,6 +64,7 @@ $(document).ready( function () {
                         if (targetDiv !== 0) {
                             if (targetDiv.value === subjectDiv.value) {
                                 subjectDiv.collideWith(targetDiv, subjectDiv, divMatrix, positionGenerator);
+                                divCnt--;
                                 thereIsAction = true;
                                 collided = true;
                             } else {
@@ -69,6 +84,8 @@ $(document).ready( function () {
 
         if (thereIsAction)
             setTimeout(spawnNewDiv, 200);
+        else if (divCnt === 16)
+            announceGameOver();
     };
 
     var collideRight = function () {
@@ -92,6 +109,7 @@ $(document).ready( function () {
                         if (targetDiv !== 0) {
                             if (targetDiv.value === subjectDiv.value) {
                                 subjectDiv.collideWith(targetDiv, subjectDiv, divMatrix, positionGenerator);
+                                divCnt--;
                                 thereIsAction = true;
                                 collided = true;
                             } else {
@@ -110,6 +128,8 @@ $(document).ready( function () {
         }
         if (thereIsAction)
             setTimeout(spawnNewDiv, 200);
+        else if (divCnt === 16)
+            announceGameOver();
     };
 
     var collideLeft = function () {
@@ -133,6 +153,7 @@ $(document).ready( function () {
                         if (targetDiv !== 0) {
                             if (targetDiv.value === subjectDiv.value) {
                                 subjectDiv.collideWith(targetDiv, subjectDiv, divMatrix, positionGenerator);
+                                divCnt--;
                                 thereIsAction = true;
                                 collided = true;
                             } else {
@@ -152,6 +173,8 @@ $(document).ready( function () {
         }
         if (thereIsAction)
             setTimeout(spawnNewDiv, 200);
+        else if (divCnt === 16)
+            announceGameOver();
     };
 
     var collideDown = function () {
@@ -175,6 +198,7 @@ $(document).ready( function () {
                         if (targetDiv !== 0) {
                             if (targetDiv.value === subjectDiv.value) {
                                 subjectDiv.collideWith(targetDiv, subjectDiv, divMatrix, positionGenerator);
+                                divCnt--;
                                 thereIsAction = true;
                                 collided = true;
                             } else {
@@ -193,29 +217,34 @@ $(document).ready( function () {
         }
         if (thereIsAction)
             setTimeout(spawnNewDiv, 200);
+        else if (divCnt === 16)
+            announceGameOver();
     };
 
 
 
     /* MAGIC STARTS DOWN HERE */
     $(document).keypress(function (key) {
-        var ascii = key.which;
+        if (!gameOver) {
+            var ascii = key.keyCode;
 
-        if (ascii === 87 || ascii === 119 || ascii === 38) {
-            /* 'W' or 'w' or 'up arrow' pressed */
-            collideUp();
-        } else if (ascii === 65 || ascii === 97 || ascii === 37) {
-            /* 'A' or 'a' or 'left arrow' pressed */
-            collideLeft();
-        } else if (ascii === 83 || ascii === 115 || ascii === 40) {
-            /* 'S' or 's' or 'down arrow' pressed */
-            collideDown();
-        } else if (ascii === 68 || ascii === 100 || ascii === 39) {
-            /* 'D' or 'd' or 'right arrow' pressed */
-            collideRight();
+            if (ascii === 87 || ascii === 119 || ascii === 38) {
+                /* 'W' or 'w' or 'up arrow' pressed */
+                collideUp();
+            } else if (ascii === 65 || ascii === 97 || ascii === 37) {
+                /* 'A' or 'a' or 'left arrow' pressed */
+                collideLeft();
+            } else if (ascii === 83 || ascii === 115 || ascii === 40) {
+                /* 'S' or 's' or 'down arrow' pressed */
+                collideDown();
+            } else if (ascii === 68 || ascii === 100 || ascii === 39) {
+                /* 'D' or 'd' or 'right arrow' pressed */
+                collideRight();
+            }
+
+            console.log(divCnt);
+            key.preventDefault();
         }
-
-        key.preventDefault();
     });
 
 });
